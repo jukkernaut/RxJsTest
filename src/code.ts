1e67f5f1214e282c244b3a0a632e4c186a832841
jukka <jukka.ollakka@ollakka.com>
2022-06-18 19:52:05 +0300

    initial

---
+++ src/code.ts
@@ -0,0 +1,5 @@
+import * as Rx from "rxjs";
+
+var d = document.getElementById("output");
+
+function log(logstring: string) {}
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+// import css from "file.css";
+
+module.exports = {
+  entry: "./src/code.ts",
+  mode: "development",
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    open: true,
+    //watchFiles: ["src/*.html"],
+    //hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      filename: "index.html",
+      title: "overriden",
+    }),
+    new CleanWebpackPlugin(),
+    // new BrowserSyncPlugin({
+    //   // browse to http://localhost:3000/ during development,
+    //   // ./public directory is being served
+    //   host: "localhost",
+    //   port: 8080,
+    //   server: { baseDir: ["dist"] },
+    // }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".tsx"],
+  },
+};
